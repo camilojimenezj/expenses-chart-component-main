886116d9293ee9c1cdef8489f498501f21975710
Camilo Jimenez <camilojj14@gmail.com>
2022-06-22 20:44:33 -0500

    style: responsive design

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 "use strict";
-let bars = document.getElementsByClassName("bar");
-let values = document.getElementsByClassName("value");
-
-fetch('./data.json')
-    .then(results => results.json())
-    .then(data => data.map(obj => obj.amount))
-    .then(data => data.map(num => (num/Math.max(...data)).toFixed(2)* 130))
-    .then(arr => {
-        bar1.style.height= arr[0] + "px";
-        bar2.style.height= arr[1] + "px";
-        bar3.style.height= arr[2] + "px";
-        bar4.style.height= arr[3] + "px";
-        bar5.style.height= arr[4] + "px";
-        bar6.style.height= arr[5] + "px";
-        bar7.style.height= arr[6] + "px";
-        bars[arr.indexOf(Math.max(...arr))].style.background = "hsl(186, 34%, 60%)"
-    });
+let bars = document.querySelectorAll(".bar");
+let values = document.querySelectorAll(".value");
 
 fetch('./data.json')
     .then(results => results.json())
@@ -24,7 +9,17 @@ fetch('./data.json')
         for(let i = 0; i < values.length; i++){
             values[i].innerHTML = "$" + data[i];
         }
+        return data
     })
+    .then(data => data.map(num => (num/Math.max(...data)).toFixed(2)* 130))
+    .then(arr => {
+        for(let i = 0; i < bars.length; i++){
+            bars[i].style.height= arr[i] + "px";
+        }
+        bars[arr.indexOf(Math.max(...arr))].style.background = "hsl(186, 34%, 60%)"
+    });
+
+
 
     
 
